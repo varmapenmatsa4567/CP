bba34df3b5c19a2a9bbce5bbd5461192fa77290f
varmapenmatsa4567 <70083124+varmapenmatsa4567@users.noreply.github.com>
2020-10-24 21:46:30 +0530

    Create BoyOrGirl(236A).cpp

---
+++ CodeForces/BoyOrGirl(236A).cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dis(a) for(auto i: a) cout<<i<<" "
+#define read(a,n) for(int i=0;i<n;i++) cin>>a[i]
+#define f(i,m,n,k) for(int i=m,i<n;i+=k)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define ll long long int
+#define si(x) scanf("%d",&x)
+#define pi(x) printf("%d",x)
+#define deb(x) cout<< #x << "="<<x<<endl
+#define all(x) x.begin(),x.end()
+const int mod = 1000000007;
+ 
+int main()
+{
+    set<char> s;
+    string k;
+    cin>>k;
+    for(int i =0;i<k.length();i++){
+        s.insert(k[i]);
+    }
+    if(s.size()%2!=0) cout<<"IGNORE HIM!";
+    else cout<<"CHAT WITH HER!"; 
+}
