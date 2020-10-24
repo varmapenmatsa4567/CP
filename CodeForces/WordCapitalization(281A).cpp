7238d414a591719fcb38e0d19334e704d0d111a0
varmapenmatsa4567 <70083124+varmapenmatsa4567@users.noreply.github.com>
2020-10-24 21:43:42 +0530

    Create WordCapitalization(281A).cpp

---
+++ CodeForces/WordCapitalization(281A).cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dis(a) for(auto i: a) cout<<i<<" "
+#define read(a,n) for(int i=0;i<n;i++) cin>>a[i]
+#define f(i,m,n,k) for(int i=m,i<n;i+=k)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define ll long long int
+#define si(x) scanf("%d",&x)
+#define pi(x) printf("%d",x)
+#define deb(x) cout<< #x << "="<<x<<endl
+#define all(x) x.begin(),x.end()
+const int mod = 1000000007;
+ 
+int main()
+{
+    string s;
+    cin>>s;
+    if(s[0]>96) s[0]-=32;
+    cout<<s;
+}
