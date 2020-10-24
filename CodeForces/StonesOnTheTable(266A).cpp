3e4d60e770d93b8df213eb5229281a49116c51f0
varmapenmatsa4567 <70083124+varmapenmatsa4567@users.noreply.github.com>
2020-10-24 21:45:06 +0530

    Create StonesOnTheTable(266A).cpp

---
+++ CodeForces/StonesOnTheTable(266A).cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dis(a) for(auto i: a) cout<<i<<" "
+#define read(a,n) for(int i=0;i<n;i++) cin>>a[i]
+#define f(i,m,n,k) for(int i=m,i<n;i+=k)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define ll long long int
+#define si(x) scanf("%d",&x)
+#define pi(x) printf("%d",x)
+#define deb(x) cout<< #x << "="<<x<<endl
+#define all(x) x.begin(),x.end()
+const int mod = 1000000007;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int c=0;
+    for(int i=0;i<n-1;i++){
+        if(s[i]==s[i+1]) c++;
+    }
+    cout<<c;
+}
