6e0cbd17680fa4a40ca37322800428871c7ae8d8
varmapenmatsa4567 <70083124+varmapenmatsa4567@users.noreply.github.com>
2020-10-24 21:50:43 +0530

    Create WrongSubtraction(977A).cpp

---
+++ CodeForces/WrongSubtraction(977A).cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dis(a) for(auto i: a) cout<<i<<" "
+#define read(a,n) for(int i=0;i<n;i++) cin>>a[i]
+#define f(i,m,n,k) for(int i=m,i<n;i+=k)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define ll long long int
+#define si(x) scanf("%d",&x)
+#define pi(x) printf("%d",x)
+#define deb(x) cout<< #x << "="<<x<<endl
+#define all(x) x.begin(),x.end()
+const int mod = 1000000007;
+ 
+int main()
+{
+    ll n,k;
+    cin>>n>>k;
+    fo(i,k){
+        if(n%10==0) n/=10;
+        else n--;
+    }
+    cout<<n;
+}
