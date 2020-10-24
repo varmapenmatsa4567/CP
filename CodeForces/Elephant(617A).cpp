a22659cde39b2ccec4fa6662a25fdf8997592a29
varmapenmatsa4567 <70083124+varmapenmatsa4567@users.noreply.github.com>
2020-10-24 21:51:34 +0530

    Create Elephant(617A).cpp

---
+++ CodeForces/Elephant(617A).cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dis(a) for(auto i: a) cout<<i<<" "
+#define read(a,n) for(int i=0;i<n;i++) cin>>a[i]
+#define f(i,m,n,k) for(int i=m,i<n;i+=k)
+#define fo(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define ll long long int
+#define si(x) scanf("%d",&x)
+#define pi(x) printf("%d",x)
+#define deb(x) cout<< #x << "="<<x<<endl
+#define all(x) x.begin(),x.end()
+const int mod = 1000000007;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%5==0) cout<<n/5;
+    else cout<<(n/5)+1;
+}
